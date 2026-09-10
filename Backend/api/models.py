be9bb6e896dcb2eb2fd4bcb1e58f4b57f98ecfe2
benoy_Judy <benoyjudy@gmail.com>
2026-09-10 15:41:53 +0530

    created the watchlist model in the backend

--- Backend/api/models.py
+++ Backend/api/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Watchlist(models.Model):
+    user= models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='watchlist'
+    )
+
+    tmdb_movie_id= models.IntegerField()
+
+    created_at=models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return f"{self.user.username}-{self.tmdb_movie_id}"
