2666ff4a7c0da28c2fc1253205fc59a5e7c23030
benoy_Judy <benoyjudy@gmail.com>
2026-09-22 12:25:02 +0530

    CORS adjustment

--- Backend/config/settings.py
+++ Backend/config/settings.py
@@ -155,5 +155,5 @@ REST_FRAMEWORK = {
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://localhost:4173",
-    "https://primetime-26.vercel.app/",
+    "https://primetime-26.vercel.app",
 ]
