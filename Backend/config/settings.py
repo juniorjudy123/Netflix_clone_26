2bfb45683987372e0f68753eee91673c8faf350b
benoy_Judy <benoyjudy@gmail.com>
2026-09-22 12:20:44 +0530

    new domain add to CORS_Allowed

--- Backend/config/settings.py
+++ Backend/config/settings.py
@@ -155,6 +155,5 @@ REST_FRAMEWORK = {
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://localhost:4173",
-    "https://netflix-clone-26-ol89j4wol-benoys-projects.vercel.app",
-    "https://netflix-clone-26-git-feature-browsepage-benoys-projects.vercel.app",
+    "https://primetime-26.vercel.app/",
 ]
